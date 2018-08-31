cb48146db602df81c90255df17eabbfa55317ba4
yuhong <yuhong@vxiaoke360.com>
2018-08-31 17:46:29 +0800

    获取用户信息，保存用户信息，清除用户信息

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,20 +1,33 @@
-
 function login(t) {
   wx.login({
     success: (res) => {
-      t.$parent.globalData.userInfo = res
-      console.log(res)
-      console.log('xxxxx', t.$parent.globalData.userInfo)
-      t.setCode(res)
+      t.$parent.globalData.userInfo = res;
+      console.log(res);
+      console.log('xxxxx', t.$parent.globalData.userInfo);
+      t.setCode(res);
     },
     fail: (res) => {
-      console.log(res)
+      console.log(res);
     },
     complete(record) {
-      console.log('complete', record)
+      console.log('complete', record);
     }
-  })
+  });
 }
+
+function saveUserInfo(userInfo) {
+  wx.setStorageSync(
+    'userInfo',
+    userInfo
+  );
+}
+
+function getUserInfo() {
+  return wx.getStorageSync('userInfo');
+}
+
 export default {
-  login
-}
+  login,
+  saveUserInfo,
+  getUserInfo
+};
