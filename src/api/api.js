3aba20bd6442c49e272d8de1af6a543e94b1f087
yuhong <yuhong@vxiaoke360.com>
2018-09-05 10:46:00 +0800

    首页标签添加下划线

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,18 @@
+import request from '../utils/request';
 
-import request from '../utils/request'
 export async function detail(goodsId) {
-  console.log(goodsId)
-  return await request(null, `api/targaryen/query_goods_detail?goodsId=` + goodsId, null)
+  console.log(goodsId);
+  return await request(null, `api/targaryen/query_goods_detail?goodsId=` + goodsId, null);
 }
+
 export async function goods(goods) {
-  return await request(null, 'v5/goods/' + goods)
+  return await request(null, 'v5/goods/' + goods);
+}
+
+export async function hometabs() {
+  return await request(null, 'api/targaryen/query_opt_list');
+}
+
+export async function goodsList(id, currentPage = 1) {
+  return await request(null, `api/targaryen/query_goods_list_by_opt_id?optId=${id}&pageNum=${currentPage}&pageSize=20&sortType=1`);
 }
